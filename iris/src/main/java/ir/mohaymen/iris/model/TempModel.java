335cd986d6bb9fb400b45b205819b0f53a418108
Kiasoh <kiarashs37@gmail.com>
2023-08-08 15:30:00 +0330

    temp services and controller removed

--- iris/src/main/java/ir/mohaymen/iris/model/TempModel.java
+++
@@ -1,5 +0,0 @@
-package ir.mohaymen.iris.model;
-
-public class TempModel {
-    String haha;
-}
--- iris/src/main/java/ir/mohaymen/iris/reposetory/PhotozRepository.java
+++
@@ -1,7 +0,0 @@
-package ir.mohaymen.iris.reposetory;
-
-import ir.mohaymen.iris.model.TempModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PhotozRepository extends CrudRepository<TempModel, Integer> {
-}
