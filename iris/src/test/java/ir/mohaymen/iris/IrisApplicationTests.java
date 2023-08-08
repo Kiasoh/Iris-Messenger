3da8145919c7492c803eec6d03b0011674291704
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-08-08 09:21:38 +0330

    project created

---
+++ iris/src/test/java/ir/mohaymen/iris/IrisApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mohaymen.iris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
