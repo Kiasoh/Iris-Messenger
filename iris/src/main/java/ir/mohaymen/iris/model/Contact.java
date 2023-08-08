37081c402030e7cfd95697aad014e81155a346a5
Kiasoh <kiarashs37@gmail.com>
2023-08-08 14:01:29 +0330

    added user and contact models

---
+++ iris/src/main/java/ir/mohaymen/iris/model/Contact.java
@@ -0,0 +1,18 @@
+package ir.mohaymen.iris.model;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+@Table(name = "contacts")
+@Data
+public class Contact {
+    @ManyToOne
+    @JoinColumn(name = "users" , referencedColumnName = "userName")
+    private User userInterest;
+    @ManyToOne
+    @JoinColumn(name = "users" , referencedColumnName = "userName")
+    private User userTarget;
+    private String firstName;
+    private String lastName;
+}
