461a9ca1d40c98dbe6e9ee66a8ad068b3cdd910b
AmirHossein <amirhossein.n2003@gmail.com>
2023-08-12 16:30:12 +0330

    add methods to DataLoader class

--- iris/src/main/java/ir/mohaymen/iris/seeder/DataLoader.java
+++ iris/src/main/java/ir/mohaymen/iris/seeder/DataLoader.java
@@ -1,18 +1,33 @@
 package ir.mohaymen.iris.seeder;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
 
     private final UserSeeder userSeeder;
+    private final ContactSeeder contactSeeder;
+    private final ChatSeeder chatSeeder;
+    private final SubscriptionSeeder subscriptionSeeder;
+    private final MediaSeeder mediaSeeder;
+    private final MessageSeeder messageSeeder;
+    private final PinSeeder pinSeeder;
+    private final UserProfileSeeder userProfileSeeder;
+    private final ChatProfileSeeder chatProfileSeeder;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         userSeeder.load();
+        contactSeeder.load();
+        chatSeeder.load();
+        subscriptionSeeder.load();
+        mediaSeeder.load();
+        messageSeeder.load();
+        pinSeeder.load();
+        userProfileSeeder.load();
+        chatProfileSeeder.load();
     }
 }
