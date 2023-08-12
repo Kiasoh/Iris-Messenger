bcd74eb9ec35be0b2158f8cb51a261952beedb30
AmirHossein <amirhossein.n2003@gmail.com>
2023-08-12 14:15:59 +0330

    remove redundant variables in UserSeeder class

--- iris/src/main/java/ir/mohaymen/iris/seeder/UserSeeder.java
+++ iris/src/main/java/ir/mohaymen/iris/seeder/UserSeeder.java
@@ -1,15 +1,10 @@
 package ir.mohaymen.iris.seeder;
 
-import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
-import com.github.javafaker.service.FakeValuesService;
-import com.github.javafaker.service.RandomService;
 import ir.mohaymen.iris.user.User;
 import ir.mohaymen.iris.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
@@ -20,24 +15,21 @@ public class UserSeeder implements Seeder {
     @Override
     public void load() {
         final int NUMBER_OF_INSTANCES = 50;
-        Locale locale = new Locale.Builder().setLanguage("en").setRegion("GB").build();
-        FakeValuesService fakeValuesService = new FakeValuesService(locale, new RandomService());
-        Faker faker = new Faker(locale);
 
         for (int i = 0; i < NUMBER_OF_INSTANCES; i++) {
-            User user = generateRandomUser(fakeValuesService, faker);
+            User user = generateRandomUser();
             userRepository.save(user);
         }
     }
 
-    private User generateRandomUser(FakeValuesService fakeValuesService, Faker faker){
-        long id= Long.parseLong(fakeValuesService.regexify("\\d{1-5}"));
+    private User generateRandomUser() {
+        long id = Long.parseLong(fakeValuesService.regexify("\\d{1-5}"));
         Name name = faker.name();
         String firstName = name.firstName();
-        String lastName = id%4==2 ?  name.lastName() : null;
+        String lastName = id % 4 == 2 ? name.lastName() : null;
         String userName = name.username() + fakeValuesService.regexify("[\\d_]{1,10}");
         String phoneNumber = faker.phoneNumber().phoneNumber();
-        String bio = id%3==1 ? fakeValuesService.regexify("[\\w\\d\\s_,\\.]{1,50}") : null;
+        String bio = id % 3 == 1 ? fakeValuesService.regexify("[\\w\\d\\s_,\\.]{1,50}") : null;
 
         User user = new User();
         user.setUserId(id);
