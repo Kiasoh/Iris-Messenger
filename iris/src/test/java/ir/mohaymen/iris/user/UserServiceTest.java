6656ce48b9d0fc148d75f478fbcf7ee3af182c3c
AmirHossein <amirhossein.n2003@gmail.com>
2023-08-14 11:20:06 +0330

    create UserServiceTest class

---
+++ iris/src/test/java/ir/mohaymen/iris/user/UserServiceTest.java
@@ -0,0 +1,13 @@
+package ir.mohaymen.iris.user;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+    @BeforeAll
+    static void setup(){
+
+    }
+}
