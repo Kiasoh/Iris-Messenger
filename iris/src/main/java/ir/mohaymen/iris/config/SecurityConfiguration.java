5d64b621bf510a0efd99436c9f9307dabbf2ebf1
hosseinb <hb3238816@gmail.com>
2023-08-15 12:44:22 +0330

    add api to profileController; fix cors;

--- iris/src/main/java/ir/mohaymen/iris/config/SecurityConfiguration.java
+++ iris/src/main/java/ir/mohaymen/iris/config/SecurityConfiguration.java
@@ -1,12 +1,9 @@
 package ir.mohaymen.iris.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,11 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
 
 @Configuration
 @EnableWebSecurity
@@ -35,8 +27,10 @@ public class SecurityConfiguration {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf()
-        .disable()
-        .authorizeHttpRequests()
+            .disable()
+            .cors()
+            .and()
+            .authorizeRequests()
         .requestMatchers(
             "/",
             "/api/auth/**",
@@ -49,7 +43,8 @@ public class SecurityConfiguration {
             "/configuration/security",
             "/swagger-ui/**",
             "/webjars/**",
-            "/swagger-ui.html")
+            "/swagger-ui.html",
+                "/upload.html")
         .permitAll()
 
         // .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(),
