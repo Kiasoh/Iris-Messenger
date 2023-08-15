216d5ddbe4995ee2f9c9029d2f345606a61ff575
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-08-15 15:11:26 +0330

    fix global exception handler

--- iris/src/main/java/ir/mohaymen/iris/config/GlobalExceptionHandler.java
+++ iris/src/main/java/ir/mohaymen/iris/config/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package ir.mohaymen.iris.handler;
+package ir.mohaymen.iris.config;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
@@ -14,34 +14,39 @@ import java.util.Arrays;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    Logger logger= LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> entityNotFound (EntityNotFoundException e) {
-        log(e);
+    public ResponseEntity<?> entityNotFound(EntityNotFoundException e) {
+        log("Not Found", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
+
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<?> nullPointer(NullPointerException e) {
-        log(e);
+        log("null pointer", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
+
     @ExceptionHandler({
-            ResponseStatusException.class ,
+            ResponseStatusException.class,
             HttpClientErrorException.class
     })
-    public ResponseEntity<?> badData (Exception e) {
-        log(e);
+    public ResponseEntity<?> badData(Exception e) {
+        log("bad data", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> hsbd(Exception e)
-    {
-        log(e);
+    public ResponseEntity<?> defaultExceptionHandler(Exception e) {
+        log("unknown", e);
         return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(e.getMessage());
     }
-    private void log(Exception e){
-        logger.error("Not found exception message:"+e.getMessage());
-        var stackTrace =Arrays.stream(e.getStackTrace()).limit(5).map(StackTraceElement::toString).toArray(String[]::new);
+
+    private void log(String msg, Exception e) {
+        logger.error(msg + " exception message:" + e.getMessage());
+        var stackTrace = Arrays.stream(e.getStackTrace()).limit(5).map(StackTraceElement::toString)
+                .toArray(String[]::new);
         logger.error(String.join("\n", stackTrace));
     }
 }
