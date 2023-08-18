2a519a9ff81ef87e8abeae190ffe6f15a46403a1
hosseinb <hb3238816@gmail.com>
2023-08-18 14:48:05 +0330

    add chat profile api; modify get profiles api;

--- iris/src/main/java/ir/mohaymen/iris/profile/ProfileController.java
+++ iris/src/main/java/ir/mohaymen/iris/profile/ProfileController.java
@@ -1,5 +1,6 @@
 package ir.mohaymen.iris.profile;
 
+import ir.mohaymen.iris.chat.Chat;
 import ir.mohaymen.iris.chat.ChatService;
 import ir.mohaymen.iris.media.Media;
 import ir.mohaymen.iris.media.MediaService;
@@ -14,11 +15,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.text.MessageFormat;
 import java.time.Instant;
-
+import java.util.List;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
@@ -29,20 +29,30 @@ public class ProfileController extends BaseController {
     private final ChatProfileService chatProfileService;
     private final UserProfileService userProfileService;
     private final UserService userService;
-    private final ChatService chatService;
     private final MediaService mediaService;
+    private final ChatService chatService;
     private final Logger logger= LoggerFactory.getLogger(ProfileController.class);
 
     @GetMapping("/users/{id}")
-    public ResponseEntity<ProfileDto> getUserProfileById(@PathVariable Long id){
-        ProfileDto profileDto = ProfileMapper.mapToProfileDto(userProfileService.getById(id));
-        return new ResponseEntity<>(profileDto, HttpStatus.OK);
+    public ResponseEntity<List<ProfileDto>> getUserProfileById(@PathVariable Long id){
+        User user = userService.getById(id);
+
+        List<ProfileDto> profiles = user.getProfiles().stream()
+                .map(profile -> ProfileMapper.mapToProfileDto(profile))
+                .toList();
+
+        return new ResponseEntity<>(profiles, HttpStatus.OK);
     }
 
     @GetMapping("/chats/{id}")
-    public ResponseEntity<ProfileDto> getChatProfileById(@PathVariable Long id){
-        ProfileDto profileDto = ProfileMapper.mapToProfileDto(chatProfileService.getById(id));
-        return new ResponseEntity<>(profileDto, HttpStatus.OK);
+    public ResponseEntity<List<ProfileDto>> getChatProfileById(@PathVariable Long id){
+        Chat chat = chatService.getById(id);
+
+        List<ProfileDto> profiles = chat.getChatProfiles().stream()
+                .map(profile -> ProfileMapper.mapToProfileDto(profile))
+                .toList();
+
+        return new ResponseEntity<>(profiles, HttpStatus.OK);
     }
 
     @RequestMapping(path = "/users", method = POST, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
@@ -52,16 +62,16 @@ public class ProfileController extends BaseController {
         Media media = Media.builder().fileMimeType(file.getContentType()).fileName(file.getOriginalFilename()).filePath("file.getResource().getURI().toString()").build();
         UserProfile userProfile = UserProfile.builder().user(user).setAt(Instant.now()).media(mediaService.createOrUpdate(media)).build();
         userProfileService.createOrUpdate(userProfile);
-        return ResponseEntity.ok("success");
+        return ResponseEntity.ok("User profile added");
     }
 
-//    @PostMapping("/chats/{id}")
-//    public ResponseEntity<String> addChatProfile(@RequestPart("file") MultipartFile file){
-//
-//    }
+    @RequestMapping(path = "/chats/{id}", method = POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<String> addChatProfile(@RequestPart("file") MultipartFile file, @PathVariable Long id){
+        Chat chat = chatService.getById(id);
+        Media media = Media.builder().fileName(file.getOriginalFilename()).filePath("not set yet").fileMimeType(file.getContentType()).build();
+        ChatProfile chatProfile = ChatProfile.builder().chat(chat).setAt(Instant.now()).media(mediaService.createOrUpdate(media)).build();
+        chatProfileService.createOrUpdate(chatProfile);
+        return ResponseEntity.ok("Chat profile added");
+    }
 
-//    @GetMapping("/profile/chats/{id}")
-//    public ResponseEntity<ProfileDto> getChatProfileById(@PathVariable Long id){
-//        ProfileDto profileDto = ProfileDto.mapToProfileDto()
-//    }
 }
