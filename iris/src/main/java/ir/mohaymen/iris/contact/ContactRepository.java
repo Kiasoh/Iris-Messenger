f9b289910d4907f522e0c8e62b0e175dc63cfe1f
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-08-20 18:05:54 +0330

    add @param for queries

--- iris/src/main/java/ir/mohaymen/iris/contact/ContactRepository.java
+++ iris/src/main/java/ir/mohaymen/iris/contact/ContactRepository.java
@@ -3,6 +3,7 @@ package ir.mohaymen.iris.contact;
 import ir.mohaymen.iris.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,14 +16,14 @@ public interface ContactRepository extends JpaRepository<Contact, Long> {
     Iterable<Contact> findBySecondUser(User secondUser);
     @Query(value = """
         select case when count(c) > 0 THEN TRUE ELSE FALSE END
-        from Contact c 
-        where c.firstUser.userId = ?1 and c.secondUser.userId = ?2
+        from Contact c
+        where c.firstUser.userId = :firstUserId and c.secondUser.userId = :secondUserId
 """)
-    boolean userInContact(Long firstUserId , Long secondUserId);
+    boolean userInContact(@Param("firstUserId") Long firstUserId ,@Param("secondUserId") Long secondUserId);
     @Query(value = """
-        select  c 
-        from Contact c 
-        where c.firstUser.userId = ?1 and c.secondUser.userId = ?2
+        select  c
+        from Contact c
+        where c.firstUser.userId = :firstUserId and c.secondUser.userId = :secondUserId
 """)
-    Contact getContact(Long firstUserId , Long secondUserId);
+    Contact getContact(@Param("firstUserId") Long firstUserId ,@Param("secondUserId") Long secondUserId);
 }
