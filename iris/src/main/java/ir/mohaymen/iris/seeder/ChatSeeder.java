43cc34c6e2dc1ecee13dbd7a878456d1602eca21
AmirHossein <amirhossein.n2003@gmail.com>
2023-08-22 09:24:09 +0330

    add owner of Chat in Seeder

--- iris/src/main/java/ir/mohaymen/iris/seeder/ChatSeeder.java
+++ iris/src/main/java/ir/mohaymen/iris/seeder/ChatSeeder.java
@@ -3,8 +3,11 @@ package ir.mohaymen.iris.seeder;
 import ir.mohaymen.iris.chat.Chat;
 import ir.mohaymen.iris.chat.ChatRepository;
 import ir.mohaymen.iris.chat.ChatType;
+import ir.mohaymen.iris.subscription.Subscription;
+import ir.mohaymen.iris.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -19,24 +22,29 @@ public class ChatSeeder implements Seeder {
     static final int NUMBER_OF_INSTANCES = 100;
     private final List<Chat> chats = new ArrayList<>();
     private final Set<String> links = new HashSet<>();
+    static final Map<Long, Set<Long>> ownerToChatMap = new HashMap<>();
 
     @Override
     public void load() {
         if (chatRepository.count() != 0) return;
 
-        for (int i = 0; i < NUMBER_OF_INSTANCES; i++)
-            generateRandomChat();
+        for (int i = 1; i <= NUMBER_OF_INSTANCES; i++)
+            generateRandomChat(i);
         chats.sort(Comparator.comparing(Chat::getCreatedAt));
         chatRepository.saveAll(chats);
     }
 
-    private void generateRandomChat() {
+    private void generateRandomChat(long chatIndex) {
         long id = faker.random().nextInt(1, 99999);
         String title = faker.book().title();
         ChatType chatType = ChatType.values()[faker.random().nextInt(1, ChatType.values().length - 1)];
         String bio = generateBio(id);
         boolean isPublic = id % 2 == 0;
         String link = (isPublic || id % 5 == 0) ? generateLink() : null;
+
+        long ownerId = faker.random().nextInt(1, UserSeeder.NUMBER_OF_INSTANCES);
+        User owner = new User();
+        owner.setUserId(ownerId);
 
         Date sendingTimeLowerBound = Date
                 .from(LocalDateTime.now(ZoneId.of("GB")).minusDays(200).atZone(ZoneId.systemDefault()).toInstant());
@@ -51,19 +59,24 @@ public class ChatSeeder implements Seeder {
         chat.setChatType(chatType);
         chat.setCreatedAt(createdTime);
         chat.setBio(bio);
+        chat.setOwner(owner);
+
+        ownerToChatMap.computeIfAbsent(owner.getUserId(), k -> new HashSet<>());
+        ownerToChatMap.get(owner.getUserId()).add(chatIndex);
 
         chats.add(chat);
     }
 
     private String generateBio(long seed) {
         if (seed % 3 == 0) return null;
-        return switch ((int) (seed % 5)) {
+        String bio = switch ((int) (seed % 5)) {
             case 0 -> faker.harryPotter().quote();
             case 1 -> faker.gameOfThrones().quote();
             case 2 -> faker.hobbit().quote();
             case 3 -> faker.dune().quote();
             default -> faker.rickAndMorty().quote();
         };
+        return (bio.length() > 255) ? bio.substring(0, 255) : bio;
     }
 
     private String generateLink() {
--- iris/src/main/java/ir/mohaymen/iris/seeder/SubscriptionSeeder.java
+++ iris/src/main/java/ir/mohaymen/iris/seeder/SubscriptionSeeder.java
@@ -16,16 +16,39 @@ public class SubscriptionSeeder implements Seeder {
     private final SubscriptionRepository subscriptionRepository;
 
     static final int NUMBER_OF_INSTANCES = 500;
-    private final List<Subscription> subscriptions = new ArrayList<>();
-    private final Map<Long, Set<Long>> userToChatMap = new HashMap<>();
+    static final List<Subscription> subscriptions = new ArrayList<>();
+    static final Map<Long, Set<Long>> userToChatMap = new HashMap<>();
 
     @Override
     public void load() {
         if (subscriptionRepository.count() != 0) return;
 
-        for (int i = 0; i < NUMBER_OF_INSTANCES; i++)
+        addOwnersToSubscription();
+        System.out.println(1);
+        for (int i = 0; i < NUMBER_OF_INSTANCES - ChatSeeder.NUMBER_OF_INSTANCES; i++)
             generateRandomSubscription();
         subscriptionRepository.saveAll(subscriptions);
+    }
+
+    private void addOwnersToSubscription() {
+        Map<Long, Set<Long>> ownerToChatMap = ChatSeeder.ownerToChatMap;
+        for (Long ownerId : ChatSeeder.ownerToChatMap.keySet()) {
+            Set<Long> chats = ChatSeeder.ownerToChatMap.get(ownerId);
+            User owner = new User();
+            owner.setUserId(ownerId);
+            for (Long chatId : chats) {
+                Chat chat = new Chat();
+                chat.setChatId(chatId);
+
+                Subscription subscription = new Subscription();
+                subscription.setUser(owner);
+                subscription.setChat(chat);
+
+                subscriptions.add(subscription);
+                userToChatMap.computeIfAbsent(ownerId, k -> new HashSet<>());
+                userToChatMap.get(ownerId).add(chatId);
+            }
+        }
     }
 
     private void generateRandomSubscription() {
