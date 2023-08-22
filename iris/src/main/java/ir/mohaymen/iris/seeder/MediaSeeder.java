26a1528188a4838c90085ea8881825099f575e49
AmirHossein <amirhossein.n2003@gmail.com>
2023-08-22 10:56:14 +0330

    generate real media for Seeder

--- iris/src/main/java/ir/mohaymen/iris/seeder/MediaSeeder.java
+++ iris/src/main/java/ir/mohaymen/iris/seeder/MediaSeeder.java
@@ -1,13 +1,25 @@
 package ir.mohaymen.iris.seeder;
 
-import com.github.javafaker.File;
+import ir.mohaymen.iris.file.FileMultipartFile;
+import ir.mohaymen.iris.file.FileService;
 import ir.mohaymen.iris.media.Media;
 import ir.mohaymen.iris.media.MediaRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItem;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
@@ -17,27 +29,43 @@ public class MediaSeeder implements Seeder {
 
     static final int NUMBER_OF_INSTANCES = 1000;
     private final List<Media> medias = new ArrayList<>();
+    private List<Path> filePaths;
+    private final FileService fileService;
 
     @Override
     public void load() {
         if (mediaRepository.count() != 0) return;
 
+        readFiles();
         for (int i = 0; i < NUMBER_OF_INSTANCES; i++)
             generateRandomMedia();
         mediaRepository.saveAll(medias);
-     }
+    }
+
+    private void readFiles() {
+        String path = "./src/main/resources/images";
+        try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+            filePaths = paths.filter(Files::isRegularFile).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private void generateRandomMedia() {
-        File file = faker.file();
-        String name = file.fileName();
-        String mimeType = file.mimeType();
-        String path = faker.letterify("./?????/???????");
+        Integer index = faker.random().nextInt(0, filePaths.size() - 1);
 
-        Media media = new Media();
-        media.setFileName(name);
-        media.setFileMimeType(mimeType);
-        media.setFilePath(path);
+        Path path = filePaths.get(index);
+        MultipartFile multipartFile = new FileMultipartFile(path);
 
+        Media media = null;
+        try {
+            media = fileService.saveFile(multipartFile.getOriginalFilename(), multipartFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (media == null) {
+            return;
+        }
         medias.add(media);
     }
 }
